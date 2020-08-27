a04835dcf38dd977a2adc38afef303a2d08010b2
Kratious <semjinghan@gmail.com>
2020-08-28 01:00:46 +0800

    Setup ESLint

--- packages/back/test/app.integration.test.js
+++ packages/back/test/app.integration.test.js
@@ -5,6 +5,6 @@ describe('app', () => {
   it('GETs / and should obtain { foo: "bar" }', async () => {
     expect.assertions(1);
     const res = await request(app).get('/').expect(200);
-    expect(res.body).toMatchInlineSnapshot(`"Hello World!"`);
+    expect(res.body).toMatchInlineSnapshot('"Hello World!"');
   });
 });
