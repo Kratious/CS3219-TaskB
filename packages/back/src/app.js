87be4866476c6bd5b50231e4ad6d597d45ac0378
Kratious <semjinghan@gmail.com>
2020-08-28 18:50:43 +0800

    Add connection to localhost

--- packages/back/src/app.js
+++ packages/back/src/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser';
 
 const app = express();
+app.use(cors()); // TODO: temporarily enable for all
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.json('Hello World!'));
--- packages/front/src/App.vue
+++ packages/front/src/App.vue
@@ -2,16 +2,31 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Hello World!" />
+    {{ info }}
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      info: null,
+    };
+  },
+  mounted() {
+    console.log(process.env.BASE_URL);
+    axios.get('http://localhost:5000/')
+      .then((response) => {
+        this.info = response.data;
+      })
+      .catch((error) => { console.log(error); });
   },
 };
 </script>
