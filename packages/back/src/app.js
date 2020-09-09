118eda8c5527d410b2d85e67bf469f18d843f18a
Kratious <semjinghan@gmail.com>
2020-09-09 20:43:49 +0800

    Add put and delete endpoints

--- packages/back/src/app.js
+++ packages/back/src/app.js
@@ -6,9 +6,10 @@ import Todo from './models/Todo';
 
 const app = express();
 app.use(cors()); // TODO: temporarily enable for all
+app.options('*', cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => Todo.query().then((todos) => {
+app.get('/', (req, res) => Todo.query().orderBy('id', 'asc').then((todos) => {
   res.json(todos);
 }));
 
@@ -18,5 +19,34 @@ app.post('/', (req, res) => Todo.query().insert({
   console.log(error);
   return res.json({ statusCode: 400 });
 }));
+
+app.put('/', (req, res) => {
+  Todo.query().findById(req.body.id).patch({
+    name: req.body.name,
+  }).then(() => res.json({ statusCode: 204 }))
+    .catch((error) => {
+      console.log(error);
+      return res.json({ statusCode: 400 });
+    });
+});
+
+app.put('/complete', (req, res) => {
+  Todo.query().findById(req.body.id).patch({
+    completed: req.body.completed,
+  }).then(() => res.json({ statusCode: 204 }))
+    .catch((error) => {
+      console.log(error);
+      return res.json({ statusCode: 400 });
+    });
+});
+
+app.delete('/', cors(), (req, res) => {
+  Todo.query().deleteById(req.query.id)
+    .then(() => res.json({ statusCode: 204 }))
+    .catch((error) => {
+      console.log(error);
+      return res.json({ statusCode: 400 });
+    });
+});
 
 module.exports = app;
