fd016207b54513fe74f95d5ccb09518dc066e358
Jing Han Sem <semjinghan@gmail.com>
2020-09-19 01:09:51 +0800

    Add tests for GET, POST, PUT, DELETE endpoints

--- packages/back/src/db/seeds/dev/todos.js
+++ packages/back/src/db/seeds/dev/todos.js
@@ -1,6 +1,12 @@
-exports.seed = (knex) => knex('todo').del()
-  .then(() => knex('todo').insert([
+exports.seed = async (knex) => {
+  await knex('todo').truncate();
+  await knex('todo').insert([
     { id: 1, name: 'Groceries for dinner' },
     { id: 2, name: 'Take the jacket to dry cleaning' },
     { id: 3, name: 'Plan weekend hike' },
-  ]));
+  ]);
+  // Note: Have two options here:
+  // 1. remove preset id from insert
+  // 2. update postgres to latest id from preset ids
+  await knex.raw('select setval(\'todo_id_seq\', max(id)) from todo');
+};
